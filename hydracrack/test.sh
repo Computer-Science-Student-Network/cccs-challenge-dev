08b211653c8b458459717d881fd076e6d9af8596
Grant Yoder <70408179+gyoder@users.noreply.github.com>
2022-11-28 14:06:33 -0700

    aaa

--- hydracrack/test.sh
+++ hydracrack/test.sh
@@ -1,4 +1,4 @@
 docker kill t-hydracrack
 docker rm t-hydracrack
 docker build -t test-hydracrack .
-docker run --name t-hydracrack -d -p 3389:22 test-hydracrack
+docker run --rm --name t-hydracrack -d -p 3389:22 test-hydracrack
